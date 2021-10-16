a6280734fe0183c5469ba766d9fbcb0e0a28edbe
Jeremiah Blanch <jblanch@nrl.com.au>
2021-10-16 23:35:42 +1100

    check recaptcha is checked before submission

--- script.js
+++ script.js
@@ -1,46 +1,57 @@
 (function (win) {
-    const GOOGLE_API = 'https://script.google.com/macros/s/AKfycbz6fIHtiqpbhuzuqQV3iRe2nvkMWWari_CBEtwVOnfs_FNyL_nMq4ORFGbq-eDXNkjBkw/exec';
-    var form = document.querySelector('form');
-    var responseEl = document.querySelector('.form-response');
-    var submitEl = document.querySelector('button[type=submit]');
-    var isSubmitting;
-    
-    form.addEventListener('submit', submitHandler);
+  const GOOGLE_API = 'https://script.google.com/macros/s/AKfycbz6fIHtiqpbhuzuqQV3iRe2nvkMWWari_CBEtwVOnfs_FNyL_nMq4ORFGbq-eDXNkjBkw/exec';
+  var form = document.querySelector('form');
+  var responseEl = document.querySelector('.form-response');
+  var submitEl = document.querySelector('button[type=submit]');
+  var isSubmitting;
 
-    var formDataToObject = (formData) => {
-        var obj = {};
-        formData.forEach((value, key) => obj[key] = value);
-        return obj;
+  form.addEventListener('submit', submitHandler);
+
+  var formDataToObject = (formData) => {
+    var obj = {};
+    formData.forEach((value, key) => obj[key] = value);
+
+    return obj;
+  }
+
+  function submitHandler(ev) {
+    var captchaResponse = grecaptcha.getResponse();
+
+    ev.preventDefault();
+    ev.stopImmediatePropagation();
+
+    if (!captchaResponse || captchaResponse.length === 0) {
+      responseEl.innerText = 'Please tick the "I’m not a robot" checkbox before you hit Send.';
+
+      return;
     }
-    function submitHandler (ev) {
-        ev.preventDefault();
-        ev.stopImmediatePropagation();
-        if (isSubmitting) {
-            return
-        }
-        isSubmitting = true;
 
-        var formData = new FormData(form);
-        var data = formDataToObject(formData);
-        console.log('form-submission', data);
-        woopra && woopra.track('form-submission', data);
-        submitEl.innerText = 'Sending...';
-        submitEl.style.opacity = 0.5;
-        
-        var req = new XMLHttpRequest();
-        req.onreadystatechange = function() { 
-            if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-                console.log('Submission successful', req.response);
-                submitEl.style.display = 'none';
-                responseEl.innerText = 'Thank you for your enquiry. We will respond to you soon.';
-                isSubmitting = false;
-            }
-            
-        }
-        req.open('POST', GOOGLE_API);
-        req.send(formData);
-
-        responseEl.innerText = '';
-        return false;
+    if (isSubmitting) {
+      return;
     }
+    isSubmitting = true;
+
+    var formData = new FormData(form);
+    var data = formDataToObject(formData);
+    console.log('form-submission', data);
+    woopra && woopra.track('form-submission', data);
+    submitEl.innerText = 'Sending...';
+    submitEl.style.opacity = 0.5;
+
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function () {
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        console.log('Submission successful', req.response);
+        submitEl.style.display = 'none';
+        responseEl.innerText = 'Thank you for your enquiry. We will respond to you soon.';
+        isSubmitting = false;
+      }
+
+    }
+    req.open('POST', GOOGLE_API);
+    req.send(formData);
+
+    responseEl.innerText = '';
+    return false;
+  }
 })(window);
